3af8523a5e6b2d07e1a04029e2e52c22d9bcf722
Sam Baker <72452805+sbaker-cs@users.noreply.github.com>
2026-05-19 16:33:21 -0400

    update auto open

--- open-adoc.ps1
+++ open-adoc.ps1
@@ -7,10 +7,17 @@ $outFile = Join-Path $env:TEMP "$baseName.html"
 $lastLine = Get-Content $FilePath -Tail 1
 $extraParams = @()
 
-if ($lastLine -match '^//\s*adoc-params:\s*(.+)$') {
+if ($lastLine -match '^//\s*open-adoc:\s*(.+)$') {
     $extraParams = $Matches[1].Trim() -split '\s+'
 }
 
 Set-Location $path
-& asciidoctor $FilePath -o $outFile @extraParams
+$errOutput = $( & asciidoctor $FilePath -o $outFile @extraParams ) 2>&1 | Where-Object { $_ -is [System.Management.Automation.ErrorRecord] }
 Start-Process $outFile
+
+if ($errOutput) {
+    if ($errOutput) {
+        $errOutput | ForEach-Object { Write-Host $_.Exception.Message -ForegroundColor Yellow }
+    }
+    Read-Host
+}
